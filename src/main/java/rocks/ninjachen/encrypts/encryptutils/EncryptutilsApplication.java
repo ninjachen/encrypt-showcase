1fa82e9aa9552a50f3e2e401bf09427095748ffa
Ninja <jiachenning@gmail.com>
2018-08-09 15:24:20 +0800

    Init commit

---
+++ src/main/java/rocks/ninjachen/encrypts/encryptutils/EncryptutilsApplication.java
@@ -0,0 +1,12 @@
+package rocks.ninjachen.encrypts.encryptutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptutilsApplication.class, args);
+	}
+}
---
+++ src/test/java/rocks/ninjachen/encrypts/encryptutils/EncryptutilsApplicationTests.java
@@ -0,0 +1,16 @@
+package rocks.ninjachen.encrypts.encryptutils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EncryptutilsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
